323d3c8620cbc64491239551ac3dd0362b13683f
shivam kushwah <kushwah.shivam30@gmail.com>
2022-02-16 13:27:23 +0530

    pro notice improved

--- notice/th-notice.php
+++ notice/th-notice.php
@@ -1,7 +1,14 @@
 <?php
 
 if (!defined('ABSPATH')){
+
     exit;
+    
+}
+
+if (class_exists('Taiowc_Pro')){
+
+   exit;
 }
 
 if ( ! class_exists( 'Taiowc_Notice' ) ){
@@ -10,8 +17,18 @@ class Taiowc_Notice{
 
     function __construct(){
 
+        if(isset($_GET['ntc-taiowc-disable']) && $_GET['ntc-taiowc-disable'] == true){
+        add_action('admin_init', array($this,'taiowc_notice_set_cookie'));
+        }
+
+        if(!isset($_COOKIE['thntc_taiowc_time'])) {
         add_action( 'admin_enqueue_scripts', array($this,'taiowc_admin_enqueue_style') );
         add_action( 'admin_notices', array($this,'taiowc_admin_notice' ));
+        }
+
+        if(isset($_COOKIE['thntc_taiowc_time'])) {
+            add_action( 'admin_notices', array($this,'taiowc_notice_unset_cookie'));
+        }
 
         
     }
@@ -22,9 +39,13 @@ class Taiowc_Notice{
 
     } 
 
-    function taiowc_admin_notice() { ?>
+    function taiowc_admin_notice() {  
 
-    <div class="taiowc-notice notice notice-success is-dismissible">
+      $display = isset($_GET['ntc-taiowc-disable'])?'none':'block';
+
+    ?>
+
+      <div class="taiowc-notice notice" style="display:<?php echo $display; ?>;">
         <div class="taiowc-notice-wrap">
             <div class="taiowc-notice-image"><img src="<?php echo esc_url( TAIOWC_PLUGIN_URI.'notice/img/cart-pro.png' );?>" alt="<?php _e('TH All In One Woo Cart Pro','taiowc'); ?>"></div>
             <div class="taiowc-notice-content-wrap">
@@ -33,11 +54,40 @@ class Taiowc_Notice{
                 <p><?php _e('Add Cross Sell , Related and Upsell products in the Cart to encourage the customer to add more products to the cart. You can also offer Coupon code in Cart','taiowc'); ?></p>
                 </div>
                 <a target="_blank" href="<?php echo esc_url('https://themehunk.com/th-all-in-one-woo-cart/');?>" class="upgradetopro-btn"><?php _e('Upgrade To Pro','taiowc');?> </a>
+
             </div>
+             <a href="?ntc-taiowc-disable=1"  class="ntc-dismiss dashicons dashicons-dismiss dashicons-dismiss-icon"></a>
         </div>
     </div>
 
     <?php }
+
+
+    function taiowc_notice_set_cookie() { 
+ 
+        $visit_time = date('F j, Y  g:i a');
+
+        $cok_time = time()+(86457*30);
+ 
+        if(!isset($_COOKIE['thntc_taiowc_time'])) {
+ 
+        // set a cookie for 1 year
+        setcookie('thntc_taiowc_time', $cok_time, time()+(86457*30));
+             
+        }
+ 
+    }
+
+    function taiowc_notice_unset_cookie(){
+
+            $visit_time = time();
+            $cookie_time = $_COOKIE['thntc_taiowc_time'];
+
+            if ($cookie_time < $visit_time) {
+                setcookie('thntc_taiowc_time', null, strtotime('-1 day'));
+            }
+    }
+
 
     
 }
