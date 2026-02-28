5b8c0e335fac9105d1cbe6f99ab6210a0c8029af
iheeresh <heereshd@gmail.com>
2026-02-28 11:36:05 +0530

    reest button issue resolved

--- inc/taiowc-setting.php
+++ inc/taiowc-setting.php
@@ -73,11 +73,6 @@ if ( ! class_exists( 'Taiowc_Set' ) ):
 				<?php esc_html_e( 'Get Premium Version', 'th-all-in-one-woo-cart' ); ?>
 			</a>
 					<p class="submit taiowc-button-wrapper th-save-btn">
-						
-						<span class="reset" href="#">
-							<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-refresh-cw" aria-hidden="true"><path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8"></path><path d="M21 3v5h-5"></path><path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16"></path><path d="M8 16H3v5"></path></svg><?php esc_html_e( 'Reset all', 'th-all-in-one-woo-cart' ); ?>
-						</span>
-
 						 <button disabled id="submit" class="button button-primary" value="<?php esc_html_e( 'Save Changes', 'th-all-in-one-woo-cart' ) ?>"><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-save transition-transform group-hover:scale-110" aria-hidden="true"><path d="M15.2 3a2 2 0 0 1 1.4.6l3.8 3.8a2 2 0 0 1 .6 1.4V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z"></path><path d="M17 21v-7a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v7"></path><path d="M7 3v4a1 1 0 0 0 1 1h7"></path></svg></span><span><?php esc_html_e( 'Save All Changes', 'th-all-in-one-woo-cart' ) ?></span>
 						 </button>
 					</p> 
