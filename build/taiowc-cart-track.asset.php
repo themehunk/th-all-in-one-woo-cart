0ce1fea3d0ebb7dfea5197106e0ad088d4507854
iheeresh <heereshd@gmail.com>
2026-07-09 18:59:56 +0530

    block style updated

--- build/taiowc-cart-track.asset.php
+++ build/taiowc-cart-track.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '1831579aa45943ceaf94');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'bd17420519295ac32dee');
